9141aa71e1f7643062256b779da4b1e5ea523ed9
abubakar471 <abdurjoy2001@gmail.com>
2024-02-11 16:30:27 +0600

    mobile responsive done

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,38 +1,53 @@
 import "./Sidebar.css"
 import React, { useState } from 'react'
 import Logo from "../../imgs/logo.png"
-import { UilSignOutAlt } from "@iconscout/react-unicons"
+import { UilSignOutAlt, UilBars } from "@iconscout/react-unicons"
 import { SidebarData } from "../../Data/Data"
+import { motion } from "framer-motion"
 
 const Sidebar = () => {
     const [selected, setSelected] = useState(0);
+    const [expanded, setExpanded] = useState(false);
 
+    const sidebarVariants = {
+        true: {
+            left: '0'
+        },
+        false: {
+            left: '-60%'
+        }
+    }
     return (
-        <div className="Sidebar">
-            {/* logo */}
-            <div className="logo">
-                <img src={Logo} alt="" />
-                <span>Dash<span>min</span></span>
+        <>
+            <div className="bars" style={expanded ? { left: "60%" } : { left: "5%" }} onClick={() => setExpanded(!expanded)}>
+                <UilBars />
             </div>
-
-            {/* menu */}
-            <div className="menu">
-                {
-                    SidebarData.map((item, index) => (
-                        <div key={index} className={selected === index ? "menuItem active" : "menuItem"}
-                            onClick={() => setSelected(index)}
-                        >
-                            <item.icon />
-                            <span>{item.heading}</span>
-                        </div>
-                    ))
-                }
-
-                <div className="menuItem">
-                    <UilSignOutAlt />
+            <motion.div variants={sidebarVariants} animate={window.innerWidth <= 768 ? `${expanded}` : ''} className="Sidebar">
+                {/* logo */}
+                <div className="logo">
+                    <img src={Logo} alt="" />
+                    <span>Dash<span>min</span></span>
                 </div>
-            </div>
-        </div>
+
+                {/* menu */}
+                <div className="menu">
+                    {
+                        SidebarData.map((item, index) => (
+                            <div key={index} className={selected === index ? "menuItem active" : "menuItem"}
+                                onClick={() => setSelected(index)}
+                            >
+                                <item.icon />
+                                <span>{item.heading}</span>
+                            </div>
+                        ))
+                    }
+
+                    <div className="menuItem">
+                        <UilSignOutAlt />
+                    </div>
+                </div>
+            </motion.div>
+        </>
     )
 }
 
